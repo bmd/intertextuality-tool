4200a6945b1dcf7de0a3397facc6a3bc1b5cc9fe
Brendan Maione-Downing <b.maionedowning@gmail.com>
2014-04-24 13:49:06 -0400

    debug imports

--- pyintertextuality/__init__.py
+++ pyintertextuality/__init__.py
@@ -1,6 +1,6 @@
 from descriptor import *
 from tokenizers import *
 from utils import *
-from stemmers import *
+from stemmers import NaiveStemmer, SchinkeStemmer
 from algorithms import *
 from compare_fingerprints import *
